48bcd3ec810cc0a146957549e19f191b78bc826d
dlampe <dirk@jac-systeme.de>
2015-09-01 19:02:55 +0200

    cjw-ezsc2015-workshop: fix for alternative session handling that works correctly in ezsc2015 virtual machine environment

--- installation/run.sh
+++ installation/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-
+# Override CJW specific session handling for ezsc2015-VM Setup
+cp -fv installation/config.yml  cjwpublish1411/cjwpublish/config/config.yml
+# diving into cjwpublish demo bundle and install it
 cd cjwpublish1411/src/Cjw/SiteCjwpublishBundle/
 sh install.sh
